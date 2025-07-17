7cdc155f7386845ef9eca78e0ac5dcf629f94541
윤동훈 <acttoze1@gmail.com>
2025-07-17 15:21:54 +0900

    FSD folder

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import { Button } from '../shared/ui/Button';
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
